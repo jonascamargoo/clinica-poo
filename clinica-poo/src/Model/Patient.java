07ec5148da46cece820dd463df7b348618894240
Jonas <jonascamargo999@gmail.com>
2024-02-28 01:34:38 -0300

    feat: add explanation for factory method

--- clinica-poo/src/Model/Patient.java
+++ clinica-poo/src/Model/Patient.java
@@ -15,20 +15,54 @@ public class Patient {
     protected String phoneNumber;
     protected long numCNS;
 
+    // Factory Method - encapsulando logica de criacao de objetos patient
+
+    // Construtor principal - cria uma instancia de Patient e itera o id
     protected Patient(
-            String name, String motherName, Date birthDate, 
-            Sex sex, Address address, String phoneNumber, 
-            boolean isCopy
-        ) {
+            String name, String motherName, Date birthDate,
+            Sex sex, Address address, String phoneNumber,
+            boolean isCopy) {
         this.name = name;
         this.motherName = motherName;
         this.birthDate = birthDate;
         this.sex = sex;
         this.address = address;
         this.phoneNumber = phoneNumber;
-        assignNumCNS(isCopy);   
+        assignNumCNS(isCopy);
     }
-    
+ 
+    // Construtor de copia -  cria uma nova instancia de  Patient a partir de outra instancia existente
+    // A motivacao para utilizar o metodo fabrica eh poder instanciar objetos com novos valores de id, sem alterar o id dos objetos subjacentes
+    public Patient(Patient patient) {
+        this(
+            patient.name, patient.motherName,
+            new Date(patient.birthDate.getTime()),
+            Sex.valueOf(patient.getSex().toString()),
+            new Address(patient.address),
+            patient.phoneNumber, true
+        );
+        long cns = patient.getNumCNS();
+        this.setId(cns);
+    }
+
+    // Construtor fabrica - valida os parametros 
+    // e cria a instancia final. Caso invalidos, retorna um null
+    public static Optional<Patient> getInstance(
+            String name, String motherName, Date birthDate,
+            Sex sex, Address address, String phoneNumber
+        ) {
+        return (
+            name != null && motherName != null &&
+                birthDate != null && sex != null &&
+                address != null && phoneNumber != null
+            ) ? Optional.of(
+                new Patient(
+                    name, motherName, birthDate, 
+                    sex, address, phoneNumber, false
+                )
+            ) : Optional.empty();
+    }
+
     private void assignNumCNS(boolean isCopy) {
         this.numCNS = isCopy ? this.numCNS : id++;
     }
@@ -40,32 +74,6 @@ public class Patient {
     public long setCNS(long cns) {
         return this.numCNS = cns;
     }
-
-    public Patient(Patient p) {
-        this(
-            p.name, p.motherName, 
-            new Date(p.birthDate.getTime()), 
-            Sex.valueOf(p.getSex().toString()),
-            new Address(p.address), 
-            p.phoneNumber, true
-        );
-        long cns = p.getNumCNS();
-        this.setId(cns);
-    }
-
-
-    public static Optional<Patient> getInstance(
-            String name, String motherName, Date birthDate, 
-            Sex sex, Address address, String phoneNumber
-        ) {
-        return (
-            name != null && motherName != null && 
-            birthDate != null && sex != null && 
-            address != null && phoneNumber != null
-        ) ? Optional.of(new Patient(name, motherName, birthDate, sex, address, phoneNumber, false)
-        ) : Optional.empty();
-    }
-
 
     public long getNumCNS() {
         return this.numCNS;
